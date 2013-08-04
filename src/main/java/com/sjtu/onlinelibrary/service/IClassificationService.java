64662fa2ac5db1f7925b74b57f3bbcfd98cca082
chenmj4444 <chenmj4444@163.com>
2013-08-04 22:27:30 +0800

    Change-Id: I6f03749b9688a56d51ae692ac318c113cd29025b

---
+++ src/main/java/com/sjtu/onlinelibrary/service/IClassificationService.java
@@ -0,0 +1,22 @@
+package com.sjtu.onlinelibrary.service;
+
+import com.sjtu.onlinelibrary.DataAccessException;
+import com.sjtu.onlinelibrary.entity.Classification;
+import com.sjtu.onlinelibrary.web.viewmodel.ClassificationEditModel;
+import com.sjtu.onlinelibrary.web.viewmodel.Pager;
+
+/**
+ * @author Crystal
+ *
+ */
+public interface IClassificationService {
+
+	void save(final Classification classification) throws DataAccessException;
+	
+    Pager<ClassificationEditModel> findAll(int pageIndex) throws DataAccessException;
+    
+    ClassificationEditModel findById(String id) throws DataAccessException;
+    
+    boolean delete(String id);
+    
+}
