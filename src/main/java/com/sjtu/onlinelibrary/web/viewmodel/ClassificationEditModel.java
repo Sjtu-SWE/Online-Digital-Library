1e56e7873a64a181ffb7a50ab81a99ee814c071c
chenmj4444 <chenmj4444@163.com>
2013-08-04 22:27:04 +0800

    Change-Id: I7579f1657f666052b2549b5de4bcf1d42c8d7287

---
+++ src/main/java/com/sjtu/onlinelibrary/web/viewmodel/ClassificationEditModel.java
@@ -0,0 +1,53 @@
+package com.sjtu.onlinelibrary.web.viewmodel;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import com.sjtu.onlinelibrary.entity.Classification;
+
+/**
+ * @author Crystal
+ *
+ */
+public class ClassificationEditModel {
+
+	private String editType;
+    private Classification classificationEntity;
+
+    public ClassificationEditModel() {
+        this("添加类别", new Classification());
+    }
+
+    public ClassificationEditModel(final String editType, final Classification classification) {
+        setEditType(editType);
+        this.classificationEntity = classification;
+    }
+
+    public Classification innerClassificationEntity() {
+        return classificationEntity;
+    }
+
+    public String getId() {
+        return innerClassificationEntity().getId();
+    }
+
+    public void setId(String id) {
+    	innerClassificationEntity().setId(id);
+    }
+
+    @NotEmpty(message = "类别名不能为空。")
+    public String getClassificationName() {
+        return innerClassificationEntity().getClassificationName();
+    }
+
+    public void setClassificationName(String name) {
+    	innerClassificationEntity().setClassificationName(name);
+    }
+
+    public String getEditType() {
+        return editType;
+    }
+
+    public void setEditType(String editType) {
+        this.editType = editType;
+    }
+
+}
