996ea6cc9d83331dc638eb76d7522acd82faf1ad
chenmj4444 <chenmj4444@163.com>
2013-08-05 13:24:23 +0800

    Change-Id: I3ef8eb6a5b828f26caa00480ffb0430544560cc0

---
+++ src/main/java/com/sjtu/onlinelibrary/web/viewmodel/UserEditModel.java
@@ -0,0 +1,128 @@
+package com.sjtu.onlinelibrary.web.viewmodel;
+
+import java.util.Date;
+import org.hibernate.validator.constraints.NotEmpty;
+import com.sjtu.onlinelibrary.entity.User;
+
+public class UserEditModel {
+
+	private String editType;
+    private User userEntity;
+
+    public UserEditModel() {
+        this("添加用户", new User());
+    }
+
+    public UserEditModel(final String editType, final User user) {
+        setEditType(editType);
+        this.userEntity = user;
+    }
+
+    public User innerUserEntity() {
+        return userEntity;
+    }
+
+    public String getId() {
+        return innerUserEntity().getId();
+    }
+
+    public void setId(String id) {
+    	innerUserEntity().setId(id);
+    }
+
+    @NotEmpty(message = "用户名不能为空。")
+    public String getUserName() {
+        return innerUserEntity().getUserName();
+    }
+
+    public void setUserName(String name) {
+    	innerUserEntity().setUserName(name);
+    }
+
+    @NotEmpty(message = "真实名不能为空。")
+    public String getRealName() {
+        return innerUserEntity().getRealName();
+    }
+
+    public void setRealName(String name) {
+    	innerUserEntity().setRealName(name);
+    }
+    
+    @NotEmpty(message = "密码不能为空。")
+    public String getPassword() {
+        return innerUserEntity().getPassword();
+    }
+
+    public void setPassword(String name) {
+    	innerUserEntity().setPassword(name);
+    }
+    
+    @NotEmpty(message = "手机号码不能为空。")
+    public String getPhone() {
+        return innerUserEntity().getPhone();
+    }
+
+    public void setPhone(String phone) {
+    	innerUserEntity().setPhone(phone);
+    }
+    
+    @NotEmpty(message = "邮箱不能为空。")
+    public String getEmail() {
+        return innerUserEntity().getEmail();
+    }
+
+    public void setEmail(String email) {
+    	innerUserEntity().setEmail(email);
+    }
+    
+    public int getCredits() {
+        return innerUserEntity().getCredits();
+    }
+
+    public void setCredits(int credits) {
+    	innerUserEntity().setCredits(credits);
+    }
+    
+    public String getEditType() {
+        return editType;
+    }
+
+    public void setEditType(String editType) {
+        this.editType = editType;
+    }
+
+    public Date getCreateDate(){
+    	return innerUserEntity().getCreateDate();
+    }
+    
+    public void setCreateDate(){
+    	if(getCreateDate() == null){
+    		innerUserEntity().setCreateDate(new Date());
+    	}
+    }
+    
+    public Date getUpdateDate(){
+    	return innerUserEntity().getUpdateDate();
+    }
+    
+    public void setUpdateDate(){
+    	innerUserEntity().setUpdateDate(new Date());
+    }
+    
+    public Date getLastLogonTime(){
+    	return innerUserEntity().getLastLogonTime();
+    }
+    
+    public void setLastLogonTime(Date lastLogonTime){
+    	innerUserEntity().setLastLogonTime(lastLogonTime);
+    }
+    
+    public String getNote(){
+    	return innerUserEntity().getNote(); 
+    }
+    
+    public void setNote(String note){
+    	innerUserEntity().setNote(note);
+    }
+
+}
