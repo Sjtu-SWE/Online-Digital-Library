0909c393de892468d60589b3e13ba1bfbe0a18a0
chenmj4444 <chenmj4444@163.com>
2013-08-16 14:54:55 +0800

    授权时根据从数据库中获取的用户的roleName字段的值来判断

--- src/main/java/com/sjtu/onlinelibrary/security/UserDetailService.java
+++ src/main/java/com/sjtu/onlinelibrary/security/UserDetailService.java
@@ -26,29 +26,21 @@ public class UserDetailService  implements UserDetailsService {
      public UserDetails loadUserByUsername(String arg0)    throws UsernameNotFoundException, DataAccessException   {
     	 String name= "";
     	 String password = "";
+    	 String roleName = "";
     	 try {
 				if(userService.findByName(arg0) != null){
-					name = userService.findByName(arg0).getUserName();//从数据库中来取  
-			    	 password = userService.findByName(arg0).getPassword();//数据库中保存的用户密码
-			    	System.out.println("=========username:"+name);
-			    	System.out.println("=========password:"+password);
+					name = userService.findByName(arg0).getUserName();//从数据库中来取
+			    	password = userService.findByName(arg0).getPassword();//数据库中保存的用户密码
+			    	roleName = userService.findByName(arg0).getRoleName();
 				}else {
 					throw new UsernameNotFoundException("User: " + arg0 + ", has no GrantedAuthority"); 
 				}
     	 } catch (Exception e) {
  			e.printStackTrace();
  		}
-    	 //ToDo:modify following
+    	 //系统管理员为ROLE_ADMIN，普通用户为ROLE_USER
          List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
-         GrantedAuthority grantedAuthority = null;
- 
-         if ("admin".equals(arg0))  {
-             grantedAuthority = new GrantedAuthorityImpl("Role_ADMIN");
-         }  else if ("manager".equals(arg0))  {
-             grantedAuthority = new GrantedAuthorityImpl("Role_MANAGER");
-         }  else    {
-             grantedAuthority = new GrantedAuthorityImpl("Role_USER");
-         }
+         GrantedAuthority grantedAuthority = new GrantedAuthorityImpl(roleName);
          grantedAuthorities.add(grantedAuthority);
  
          User user = new User(arg0, password, true, true, true, true, grantedAuthorities);
