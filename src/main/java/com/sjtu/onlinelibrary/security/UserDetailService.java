371692af3a14d40324edf445d7026199a183ef0a
chenmj4444 <chenmj4444@163.com>
2013-08-20 11:57:26 +0800

    返回的User使用自定义的Entity，而不是Spring Security自带的User

--- src/main/java/com/sjtu/onlinelibrary/security/UserDetailService.java
+++ src/main/java/com/sjtu/onlinelibrary/security/UserDetailService.java
@@ -8,8 +8,9 @@ import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.sjtu.onlinelibrary.entity.User;
 import com.sjtu.onlinelibrary.service.IUserService;
-import org.springframework.security.core.userdetails.User;
 
 public class UserDetailService  implements UserDetailsService {
 
@@ -29,21 +30,23 @@ public class UserDetailService  implements UserDetailsService {
     	 String roleName = "";
     	 try {
 				if(userService.findByName(arg0) != null){
-					name = userService.findByName(arg0).getUserName();//从数据库中来取
-			    	password = userService.findByName(arg0).getPassword();//数据库中保存的用户密码
-			    	roleName = userService.findByName(arg0).getRoleName();
+					User temp = userService.findByName(arg0);
+					name = temp.getUsername();//从数据库中取出
+			    	password = temp.getPassword();//数据库中保存的用户密码
+			    	roleName = temp.getRoleName();
 				}else {
 					throw new UsernameNotFoundException("User: " + arg0 + ", has no GrantedAuthority"); 
 				}
     	 } catch (Exception e) {
  			e.printStackTrace();
  		}
+    			 
     	 //系统管理员为ROLE_ADMIN，普通用户为ROLE_USER
          List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
          GrantedAuthority grantedAuthority = new GrantedAuthorityImpl(roleName);
          grantedAuthorities.add(grantedAuthority);
- 
-         User user = new User(arg0, password, true, true, true, true, grantedAuthorities);
+         
+         User user = new User(name, password, true, true, true, true, grantedAuthorities);
          return user;
      }
 
